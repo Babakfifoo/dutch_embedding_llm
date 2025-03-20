c04b8d6c73fd908204de1e476d73dc6ad28fd074
Babak Firoozi Fooladi <b.firoozi.f@gmail.com>
2025-03-20 08:31:08 +0200

    Final results are cleaned and ready to use.

--- src/10_process_llm_answers.py
+++ src/10_process_llm_answers.py
@@ -1,7 +1,8 @@
+# %%
+
 import json
 import pandas as pd
 from pathlib import Path
-
 
 def bool_cleaner(s: str) -> bool:
     # validating the answer
@@ -10,6 +11,11 @@ def bool_cleaner(s: str) -> bool:
     if "true" in s.lower():
         return True
     return False
+
+
+def bool_cleaner_string(s):
+    s = json.loads(s)
+    return s.get("answer", None)
 
 
 def get_answers(res_fp, ans_fp):
@@ -24,108 +30,84 @@ def get_answers(res_fp, ans_fp):
     with open(file=ans_fp) as f:
         ans = json.loads(f.read())
         ans = pd.json_normalize(ans)[["IMRO", "answer"]]
-        ans["answer"] = ans["answer"].apply(bool_cleaner)
+        ans["answer"] = ans["answer"].apply(bool_cleaner_string)
 
     df = res.merge(ans, on="IMRO", how="outer")
     return df
 
 
+def clean_results(
+    name: str, res_fp: str, ins_fp: str, ans_fp: str, output: str
+) -> None:
+    with open(file=ins_fp) as f:
+        ins_data = json.loads(f.read())
+        ins_data = pd.DataFrame({f"{name}_inspection": ins_data})
+
+    with open(file=ans_fp) as f:
+        ans_data = json.loads(f.read())
+        ans_data = pd.DataFrame(ans_data).set_index("IMRO")
+        ans_data[f"{name}_llm"] = ans_data["answer"].apply(bool_cleaner_string)
+        ans_data = ans_data[[f"{name}_llm"]]
+
+    with open(file=res_fp) as f:
+        res_data = json.loads(f.read())
+        res_data = pd.DataFrame({f"{name}_result": res_data})
+
+    df = ans_data.join(res_data, how="left").join(ins_data, how="left")
+    df.to_parquet(output)
+
+
 # %%
+clean_results(
+    name="anterior_agreement",
+    res_fp="../data/plan_documents/results/inspecting_false/result_anterior_20250317.json",
+    ans_fp="../data/plan_documents/answered/anterior agreement.json",
+    ins_fp="../data/plan_documents/results/manual_inspection/inspection_anterior_20250317.json",
+    output="../data/final/01_Anterior_agreement.parquet",
+)
 
-res_fp: str = "../data/plan_documents/results/result_allocation - 20250311.json"
-ans_fp: str = "../data/plan_documents/answered/Land allocation.json"
+clean_results(
+    name="land_allocation",
+    res_fp="../data/plan_documents/results/inspecting_false/result_allocation_20250318.json",
+    ans_fp="../data/plan_documents/answered/Land allocation.json",
+    ins_fp="../data/plan_documents/results/manual_inspection/inspection_land_allocation_20250318.json",
+    output="../data/final/02_Land_allocation.parquet",
+)
 
-land_alloc = get_answers(res_fp=res_fp, ans_fp=ans_fp)
+clean_results(
+    name="municipality_land",
+    res_fp="../data/plan_documents/results/inspecting_false/result_municipality_land_20250318.json",
+    ans_fp="../data/plan_documents/answered/municipality ownership.json",
+    ins_fp="../data/plan_documents/results/manual_inspection/inspection_municipality_20250318.json",
+    output="../data/final/03_municipality_land.parquet",
+)
 
-res_fp: str = "../data/plan_documents/results/result_anterior - 20250311.json"
-ans_fp: str = "../data/plan_documents/answered/anterior agreement.json"
+clean_results(
+    name="exploitation",
+    res_fp="../data/plan_documents/results/inspecting_false/result_operating_20250318.json",
+    ans_fp="../data/plan_documents/answered/operating agreement.json",
+    ins_fp="../data/plan_documents/results/manual_inspection/inspection_operating_20250318.json",
+    output="../data/final/04_exploitation_agreement.parquet",
+)
 
-anterior = get_answers(res_fp=res_fp, ans_fp=ans_fp)
+clean_results(
+    name="plan_damage",
+    res_fp="../data/plan_documents/results/inspecting_false/result_plan_damage_20250318.json",
+    ans_fp="../data/plan_documents/answered/plan damage agreement.json",
+    ins_fp="../data/plan_documents/results/manual_inspection/inspection_plan_damage_20250318.json",
+    output="../data/final/05_plan_damage_agreement.parquet",
+)
 
-res_fp: str = "../data/plan_documents/results/result_building_plan - 20250311.json"
-ans_fp: str = "../data/plan_documents/answered/building Plan.json"
-
-building_plan = get_answers(res_fp=res_fp, ans_fp=ans_fp)
-
-res_fp: str = "../data/plan_documents/results/result_cooperation - 20250311.json"
-ans_fp: str = "../data/plan_documents/answered/cooperation agreement.json"
-
-coop = get_answers(res_fp=res_fp, ans_fp=ans_fp)
-
-res_fp: str = "../data/plan_documents/results/result_land_exp_scheme - 20250311.json"
-ans_fp: str = "../data/plan_documents/answered/land exploitation scheme.json"
-
-land_exp_scheme = get_answers(res_fp=res_fp, ans_fp=ans_fp)
-
-res_fp: str = "../data/plan_documents/results/result_municipality_land - 20250312.json"
-ans_fp: str = "../data/plan_documents/answered/municipality ownership.json"
-
-municipality_land = get_answers(res_fp=res_fp, ans_fp=ans_fp)
-
-
-res_fp: str = "../data/plan_documents/results/result_operating - 20250311.json"
-ans_fp: str = "../data/plan_documents/answered/operating agreement.json"
-
-operating = get_answers(res_fp=res_fp, ans_fp=ans_fp)
-
-
-res_fp: str = "../data/plan_documents/results/result_plan_damage - 20250311.json"
-ans_fp: str = "../data/plan_documents/answered/plan damage agreement.json"
-
-damage = get_answers(res_fp=res_fp, ans_fp=ans_fp)
-
-
-res_fp: str = "../data/plan_documents/results/result_developer_land - 20250312.json"
-ans_fp: str = "../data/plan_documents/answered/private ownership.json"
-
-dev_land = get_answers(res_fp=res_fp, ans_fp=ans_fp)
-
-res_fp: str = "../data/plan_documents/results/result_purchase - 20250311.json"
-ans_fp: str = "../data/plan_documents/answered/purchase agreement.json"
-
-purchase = get_answers(res_fp=res_fp, ans_fp=ans_fp)
-
-res_fp: str = "../data/plan_documents/results/result_realisation - 20250311.json"
-ans_fp: str = "../data/plan_documents/answered/realisation agreement.json"
-
-realisation = get_answers(res_fp=res_fp, ans_fp=ans_fp)
-
-res_fp: str = "../data/plan_documents/results/result_space_for_space - 20250311.json"
-ans_fp: str = "../data/plan_documents/answered/space for space.json"
-
-space_for_space = get_answers(res_fp=res_fp, ans_fp=ans_fp)
-# %%
-
-with open("../data/plan_documents/manual_answers/html_manual - 20250226.json") as f:
-    manual = json.loads(f.read())
-    manual = (
-        pd.DataFrame(manual)
-        .T.reset_index()
-        .rename(
-            columns={
-                "index": "IMRO",
-                "Land allocation":"alloc_man"
-            }
-        )
-    ).reset_index(drop=True)
-    
-    manual = manual[manual["Invalid"] == False]
-    
-# %%
+clean_results(
+    name="cooperation",
+    res_fp="../data/plan_documents/results/inspecting_false/result_cooperation_20250318.json",
+    ans_fp="../data/plan_documents/answered/cooperation agreement.json",
+    ins_fp="../data/plan_documents/results/manual_inspection/inspection_coop_20250318.json",
+    output="../data/final/06_cooperation_agreement.parquet",
+)
 
 all_plans = pd.DataFrame(
     {"IMRO": [str(f.stem) for f in list(Path("../data/plan_documents/md/").iterdir())]}
 )
-# %%
-
-land_allocation_df = (
-    all_plans
-    .merge(land_alloc, on="IMRO", how="left")
-    .merge(manual[["IMRO", "alloc_man"]], on="IMRO", how="left")
-    )
-
-test = land_allocation_df.dropna()
-
-test[test.answer != test.alloc_man][["IMRO"]]
-
+all_plans.to_parquet("../data/final/00_all_plans.parquet")
 # %%
